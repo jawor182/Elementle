f87759eec2699e793e7b5a4db13de35c8d83505c
jawor182 <kubajaworski59@outlook.com>
2024-01-06 16:29:54 +0100

    Stworzenie Strony glownej

--- app/page.jsx
+++ app/page.jsx
@@ -1,43 +1,43 @@
 import "./globals.css"
-import Image from "next/image";
 import Header from "@/components/Header";
+import backgroundImage from "/public/bg.png"
 import styles from "../components/styles/Page.module.css"
 export default function Home() {
     return (
         <div className="app">
             <Header />
-            
-            <div className="Center"
+            <div
                 style={{
-                    position: "relative",
+                    backgroundImage: `url(${backgroundImage.src})`, //.src odpowiada za sciezke do pliku
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
                     width: "100vw",
                     height: "100vh",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                     backgroundColor:"#457B9D"
-                }}>
-                    <div className={styles.wrapper}>
-                        <div className={styles.minigra}>
-                            <div className={styles.block}></div>
-                        </div>
-                        <div className={styles.minigra}>
-                            <div className={styles.block}></div>
-                        </div>
-                        <div className={styles.minigra}>
-                            <div className={styles.block}></div>
-                        </div>
-                        <div className={styles.minigra}>
-                            <div className={styles.block}></div>
-                        </div>
-
+                }}
+            >
+                <div className={styles.wrapper}>
+                    <div className={styles.minigra}>
+                        <div className={styles.block}></div>
                     </div>
-                <Image
-                    src="/bg.png"
-                    alt="Mountains with snow"
-                    layout="fill"
-                    objectFit="cover"
-                />
+                    <div className={styles.minigra}>
+                        <div className={styles.block}></div>
+                    </div>
+                    <div className={styles.minigra}>
+                        <div className={styles.block}></div>
+                    </div>
+                    <div className={styles.minigra}>
+                        <div className={styles.block}></div>
+                    </div>
 
-                
-            </div>           
+                </div>
+             
+
+            </div>
         </div>
     );
 }
