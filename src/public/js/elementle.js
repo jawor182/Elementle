e47426e4ec2212246e3c6ccf35939dc8e2a97ff4
jawor182 <kubajaworski59@outlook.com>
2024-02-16 17:22:14 +0100

    Progres ewaluacji inputu

--- src/public/js/elementle.js
+++ src/public/js/elementle.js
@@ -1,5 +1,16 @@
+const resultItemRodzaj = document.getElementById("resultItemRodzaj");
+const resultItemMasaAtomowa = document.getElementById("resultItemMasaAtomowa");
+const resultItemRokOdkrycia = document.getElementById("resultItemRokOdkrycia");
+const resultItemOkres = document.getElementById("resultItemOkres");
+const resultItemElektroujemnosc = document.getElementById(
+  "resultItemElektroujemnosc"
+);
+const resultItemRodzajWartosciowosc = document.getElementById(
+  "resultItemRodzajWartosciowosc"
+);
+const elementinput = document.getElementById("elementinput");
+const resultsBlock = document.getElementById("resultsBlock");
 async function fetchData(event) {
-  console.log("Fetch works!")
   event.preventDefault();
   const elementInput = document.getElementById("elementinput");
   const formData = elementInput.value;
@@ -10,15 +21,30 @@ async function fetchData(event) {
     },
     body: JSON.stringify({ formData }),
   })
-  .then((response) => response.json())
-  .then((data) => {
-    const resultsBlock = document.getElementById("resultsBlock");
-    resultsBlock.textContent = JSON.stringify(data.formData);
-    console.log(data.formData); 
-    console.log(data.mockPierwiastekInput); 
-    console.log(data.mockPierwiastekTodays); 
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      //resultsBlock.textContent = JSON.stringify(data.formData);
+      const formData = JSON.stringify(data.formData);
+      const formDataObject = JSON.parse(formData);
+      const input = formDataObject.formData; // Corrected variable name
+      const answer = data.mockPierwiastekTodays;
+      const guess = data.mockPierwiastekInput;
+      guess.nazwa = input;
+      console.log(input);
+      console.log("Guess:", guess.nazwa);
+      console.log("Answer:", answer.nazwa);
+
+      
+      
+      if (guess.nazwa.toLowerCase() === answer.nazwa.toLowerCase()) {
+        console.log("Works");
+      } else {
+        console.log("Not works");
+      }
+
+      //elementInput.value = "";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
