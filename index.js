e4e2c830d83101a29e7273a8750217f7467c9d03
jawor182 <kubajaworski59@outlook.com>
2024-02-18 14:06:15 +0100

    Dodanie HTMX zamiast JS

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/** @format */
-
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -26,7 +24,7 @@ app.get("/znajdowanie", (req, res) => {
   res.sendFile(__dirname + "/src/znajdowanie.html");
 });
 app.post("/elementle", (req, res) => {
-  const formData = req.body;
+  const formData = req.body.element;
   function MockPierwiastek({
     nazwa,
     rodzaj,
@@ -62,10 +60,37 @@ app.post("/elementle", (req, res) => {
     elektroujemnosc: 1,
     wartosciowosc: 1,
   });
-  //console.log(mockPierwiastekTodays);
-  res.json({ formData, mockPierwiastekInput, mockPierwiastekTodays });
-});
+  let results = [];
+  if(mockPierwiastekInput.nazwa.toLowerCase() === mockPierwiastekTodays.nazwa.toLowerCase()){
+    results.push({
+      nazwa: mockPierwiastekTodays.nazwa,
+      rodzaj: mockPierwiastekTodays.rodzaj,
+      masaAtomowa: mockPierwiastekTodays.masaAtomowa,
+      rokOdkrycia: mockPierwiastekTodays.rokOdkrycia,
+      okres: mockPierwiastekTodays.okres,
+      elektroujemnosc: mockPierwiastekTodays.elektroujemnosc,
+      wartosciowosc: mockPierwiastekTodays.wartosciowosc
+    });
+  }
 
+  // Convert the array of results to HTML
+  let htmlSnippet = '';
+  results.forEach(result => {
+    htmlSnippet += `
+      <div class="resultsWrapper">
+        <div class="resultCorrect">${result.nazwa}</div>
+        <div class="resultCorrect">${result.rodzaj}</div>
+        <div class="resultCorrect">${result.masaAtomowa}</div>
+        <div class="resultCorrect">${result.rokOdkrycia}</div>
+        <div class="resultCorrect">${result.okres}</div>
+        <div class="resultCorrect">${result.elektroujemnosc}</div>
+        <div class="resultCorrect">${result.wartosciowosc}</div>
+      </div>
+    `;
+  });
+
+  res.send(htmlSnippet);
+});
 app.listen(process.env.PORT, () => {
   console.log(`App is listening on http://localhost:${process.env.PORT}`);
 });
